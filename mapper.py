8f3ec7c139e945c2f7c27a209a09c6f6ed4a6d8d
Siddharth Panigrahi <siddharth7997@gmail.com>
2018-09-05 10:57:16 +0530

    changes in domain

---
+++ mapper.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+import sys
+import re
+for line in sys.stdin:
+    line = line.strip() #remove leading and trailing whitespaces
+    # words = line.split("\"") #split the line into words and returns as a list
+    # for m in words:
+    #     if "https://" in m or "http://" in m:
+    #         print m,"\t",1
+    urls = re.findall('https?://(?:[-\w.])+',line)
+    if urls:
+        print urls[0],"\t",1
+
